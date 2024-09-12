18ecd4961fb235456f1dc3a874620278771a4e6d
Omar <omarkhaled1752003@yahoo.com>
2024-09-12 17:02:53 +0300

    added creation date, and a method to check if the otp code is valid

--- otp/models.py
+++ otp/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 
 class OTPCode(models.Model):
     phone = models.CharField(max_length=30)
     code = models.CharField(max_length=4)
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def is_valid(self):
+        return timezone.now() < self.created_at + timedelta(minutes=5)
