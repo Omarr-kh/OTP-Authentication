2b19153beb9b20150027943fbded26997fc0f9f4
Omar <omarkhaled1752003@yahoo.com>
2024-09-12 16:37:25 +0300

    created login view

--- otp/views.py
+++ otp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
+from .models import OTPCode
+
 import traceback
 import random
 
@@ -35,13 +37,37 @@ def register_user(request):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        user = User.objects.create(
-            username=username, email=email, password=password
-        )
+        user = User.objects.create(username=username, email=email, password=password)
         Token.objects.create(user=user)
         return Response(
-            {"message": f"User registered successfully for {user.username}!"}, status=status.HTTP_201_CREATED
+            {"message": f"User registered successfully for {user.username}!"},
+            status=status.HTTP_201_CREATED,
         )
+    except:
+        print(traceback.format_exception)
+        return Response(status=status.HTTP_409_CONFLICT)
+
+
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def login_user(request):
+    try:
+        email = request.data.get("email")
+        password = request.data.get("password")
+        if not email or not password:
+            return Response(
+                {"error": "email, and password are required!"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if not User.objects.filter(email=email, password=password).exists():
+            return Response(
+                {"error": "Username or password are incorrect!"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        user = User.objects.get(email=email, password=password)
+        token = Token.objects.get(user=user)
+
+        return Response({"token": {token.key}})
     except:
         print(traceback.format_exception)
         return Response(status=status.HTTP_409_CONFLICT)
