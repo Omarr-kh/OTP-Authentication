53239f43f0d364072fa35025cf446be4ad35a29f
Omar <omarkhaled1752003@yahoo.com>
2024-09-12 16:37:33 +0300

    url for login

--- otp/urls.py
+++ otp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, register_user
+from .views import home_page, register_user, login_user
 
 urlpatterns = [
     path("home/", home_page, name="home"),
     path("register/", register_user, name="register"),
+    path("login/", login_user, name="login"),
 ]
