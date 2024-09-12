62612989aa170dbda1cc9cdaa8b793a8f36c4726
Omar <omarkhaled1752003@yahoo.com>
2024-09-12 17:03:26 +0300

    urls to handel otp

--- otp/urls.py
+++ otp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home_page, register_user, login_user
+from .views import home_page, register_user, login_user, send_otp, check_otp
 
 urlpatterns = [
     path("home/", home_page, name="home"),
     path("register/", register_user, name="register"),
     path("login/", login_user, name="login"),
+    path("send-otp/", send_otp, name="send-otp"),
+    path("check-otp/", check_otp, name="check-otp"),
 ]
